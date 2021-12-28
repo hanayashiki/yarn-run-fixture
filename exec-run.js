bb5ddcce6b58a69fc28bcb5f9d367209b9cc7e60
hanayashiki <cwang592@usc.edu>
2021-12-28 22:01:31 +0800

    push

--- exec-run.js
+++ exec-run.js
@@ -1,3 +1,9 @@
 const subprocess = require('child_process')
 
-subprocess.exec('yarn serve').stdout.pipe(process.stdout)
+const process = subprocess.exec('yarn serve');
+process.stdout.pipe(process.stdout)
+
+setTimeout(() => {
+  console.log('killing process')
+  process.kill()
+}, 3000);
